792cb0e8adb9f050497995d93bafbe911b716a73
Vladyslav Honchar <vladyslav.honchar@tangramltd.com>
2025-05-25 17:47:20 +0000

    (wishlist) Complete wishlist development

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,1 +1,27 @@
 //Логіка сторінки Wishlist
+import { fetchAllData, setTotalElements, setTotalPrice } from './js/helpers.js';
+import { refs } from './js/refs.js';
+import { renderNotFound, renderProducts } from './js/render-function.js';
+import { useLocalStorage } from './js/storage.js';
+import { CART_KEY, WISHLIST_KEY } from './js/constants.js';
+
+
+
+
+
+export async function renderData() {
+  const wishlistData = await fetchAllData(WISHLIST_KEY);
+  const [cartData] = useLocalStorage(CART_KEY)
+  if (Array.isArray(wishlistData) && wishlistData.length) {
+    renderNotFound(false)
+    renderProducts(wishlistData)
+
+    setTotalElements(refs.headerCartSummary,cartData)
+    setTotalElements(refs.wishlistHeaderSummary,wishlistData)
+  } else {
+    renderNotFound(true)
+  }
+}
+
+
+renderData()
